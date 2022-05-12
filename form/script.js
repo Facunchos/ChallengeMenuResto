a8ed00d68826341e0fee18783844c9b95938eb35
MatiasAltmann <102705202+MatiasAltmann@users.noreply.github.com>
2022-05-11 22:52:56 -0300

    corrección

--- form/script.js
+++ form/script.js
@@ -5,45 +5,74 @@ function validarFormulario() {
     let celular = document.getElementById('celular');
     let opciones = document.getElementsByClassName('opcion');
   
+    checkearErrores(nombre,asunto,email,celular,opciones);
 
-    var mensajesError = [];
-    if(nombre.value.length === 0){ 
-        mensajesError.push('Error. Completá tu nombre <br>');
-    }
-
-    if(email.value.length === 0) {
-        mensajesError.push('Error. Completá tu email <br>');
-    }
-
-   
-    if(asunto.value.length === 0){ 
-        mensajesError.push('Error. Completá tu asunto <br>');
 
     }
 
-    if(celular.value.length != 10){ 
-        mensajesError.push('Error. Verificá que tu número de celular tengo 10 números <br>');
+ function checkearErrores(nombre, asunto, email, celular, opciones){
+        let errores = false;
 
-    }
+        var mensajesError = [];
 
-    let cliceado = false;
-    let i = 0;
-
-    while(i < opciones.length){
-        if (opciones[i].checked){
-            cliceado = true;
+        if(nombre.value.length === 0){ 
+            mensajesError.push('Error. Completá tu nombre <br>');
+            errores = true;
+           
         }
-        i++;
-    }
+    
+        if(email.value.length === 0) {
+            mensajesError.push('Error. Completá tu email <br>');
+            errores = true;
+        }
+    
+       
+        if(asunto.value.length === 0){ 
+            mensajesError.push('Error. Completá tu asunto <br>');
+            errores = true;
+        }
 
-    if(cliceado == false){
-        mensajesError.push('Error. No hay tipo de comensal seleccionado');
+        if(validarFormatoTelefono(celular)){
+            mensajesError.push('Telefono no valido <br>' );
+            errores = true;
+        }
 
-    }
+    
+    
+        let clickeado = false;
+        let i = 0;
+    
+        while(i < opciones.length){
+            if (opciones[i].checked){
+                clickeado = true;
+            }
+            i++;
+        }
+    
+        if(clickeado == false){
+            mensajesError.push('Error. No hay tipo de comensal seleccionado');
+            errores = true;
+        }
+    
+        document.getElementById('errorDatos').innerHTML = mensajesError.join('');
 
-    document.getElementById('errorDatos').innerHTML = mensajesError.join('');
-
+        if(errores == false){
+            alert(nombre.value + ' tu mensaje fue enviado. Te contactaremos a la brevedad');
+        }
+    
 }
+
+
+function validarFormatoTelefono(celular){
+    let resultado = false;
+    var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    if(!celular.value.match(phoneno)){
+        resultado = true;
+    }
+    return resultado;
+}
+
+    
 
 function removerError() {
     document.getElementById('errorDatos').innerHTML = ""; 
